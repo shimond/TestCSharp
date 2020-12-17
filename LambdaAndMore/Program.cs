861f00be1622e4ee6f3ba573baab5be8309e06a3
Shimon Dahan <Shimond@any-techs.co.il>
2020-12-17 10:17:16 +0200

    init

---
+++ LambdaAndMore/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaAndMore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Action> actions = new();
+            for (int i = 0; i < 5; i++)
+            {
+                actions.Add(() => Console.WriteLine($"Action {i}"));
+            }
+
+            foreach (var action in actions)
+            {
+                action();
+            }
+        }
+    }
+}
