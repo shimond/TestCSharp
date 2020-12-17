bee26029b69bcad783b2fa0ba3a7d4f1080f641d
Shimon Dahan <Shimond@any-techs.co.il>
2020-12-17 10:57:08 +0200

    Using and memory

--- LambdaAndMore/Program.cs
+++ LambdaAndMore/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.IO;
 namespace LambdaAndMore
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            // WhatIsTheOutput();
+            using (StreamReader reader = new StreamReader("FILE_PATH"))
+            {
+                // read from file
+                reader.Close();
+            }
+        }
+
+        private static void WhatIsTheOutput()
         {
             List<Action> actions = new();
             for (int i = 0; i < 5; i++)
