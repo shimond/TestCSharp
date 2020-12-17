40c5d792d4014d573d61ffff3f4dc5931357f897
Shimon Dahan <Shimond@any-techs.co.il>
2020-12-17 09:43:29 +0200

    FirstInit

---
+++ TestAsync/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main NO.1");
+            _ = StartIt();
+            Console.WriteLine("Main NO.2");
+            Console.ReadLine();
+        }
+
+        private static async Task StartIt()
+        {
+            Console.WriteLine("StartIt 1");
+            await MethodA();
+            Console.WriteLine("StartIt 2");
+        }
+
+        static async Task MethodA()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                await Task.Delay(2000);
+                Console.WriteLine($"Method A {i}");
+            }
+        }
+
+    }
+}
---
+++ TestCancel/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestCancel
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            txtLogs.Clear();
+            AddToLog("btnStart_Click Started.");
+            await MethodA();
+            AddToLog("btnStart_Click Finished.");
+        }
+
+        private async Task MethodA()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                await Task.Delay(2000);
+                AddToLog($"Method A {i}");
+            }
+        }
+
+        private void AddToLog(string str)
+        {
+            txtLogs.AppendText($"{str}{Environment.NewLine}");
+        }
+
+
+    }
+}
